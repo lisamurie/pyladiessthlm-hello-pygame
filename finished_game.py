6bb6fb012262689b072a4d275e9ca5b26bfef1d5
Linnéa Löfdahl <linnea@nekomata.se>
2013-10-16 20:37:55 +0200

    Game complete, how can it be simplified?

--- finished_game.py
+++ finished_game.py
@@ -1,50 +1,39 @@
-import pygame, sys, os
+import pygame, random
 from pygame.locals import *
 
-
-#functions to create our resources
-def load_image(name, colorkey=None):
-    fullname = os.path.join('', name)
-    try:
-        image = pygame.image.load(fullname)
-    except pygame.error, message:
-        print 'Cannot load image:', fullname
-        raise SystemExit, message
-    image = image.convert()
-    if colorkey is not None:
-        if colorkey is -1:
-            colorkey = image.get_at((0,0))
-        image.set_colorkey(colorkey, RLEACCEL)
-    return image, image.get_rect()
-
 class Kitten(pygame.sprite.Sprite):
-    def __init__(self):
-    	pygame.sprite.Sprite.__init__(self) #call Sprite intializer
-        self.image, self.rect = load_image('150.jpg', -1)
-        screen = pygame.display.get_surface()
-        self.area = screen.get_rect()
-        self.rect.topleft = 10, 10
-        self.move = 9
-        self.dizzy = 0
+  def __init__(self):
+    pygame.sprite.Sprite.__init__(self) #call Sprite intializer
+    self.image = pygame.image.load('150.jpg')
+    self.rect = self.image.get_rect()
+    self.rect.center = (250, 250)
 
+  def run_away(self):
+    mouse_position = pygame.mouse.get_pos()
+    if self.rect.collidepoint(mouse_position):
+      self.rect.move_ip(random.uniform(-25, 25), random.uniform(-25, 25))
 
 pygame.init()
-screen = pygame.display.set_mode((450, 450))
+screen = pygame.display.set_mode((500, 500))
+pygame.display.flip()
 
-#Create The Backgound
 background = pygame.Surface(screen.get_size())
 background = background.convert()
 background.fill((250, 250, 250))
 
-#Display The Background
-screen.blit(background, (0, 0))
-pygame.display.flip()
+clock = pygame.time.Clock()
 
 kitten = Kitten()
 allsprites = pygame.sprite.RenderPlain((kitten))
 
+pygame.mouse.set_visible(1)
+
 while 1:
-  #Draw Everything
+  #Handle Input Events
+  for event in pygame.event.get():
+    if event.type == MOUSEMOTION:
+      kitten.run_away()
+
   screen.blit(background, (0, 0))
-  allsprites.draw(screen)
-  pygame.display.flip()
+  allsprites.draw(screen) 
+  pygame.display.flip() 
